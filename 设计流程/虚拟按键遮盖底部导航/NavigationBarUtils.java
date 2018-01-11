dbffa93e838c726d652e4bc533659b115d9ed34c
c297131019 <297131019@qq.com>
2018-01-11 17:50:55 +0800

    更新一下解决方案

---
+++ 设计流程/虚拟按键遮盖底部导航/NavigationBarUtils.java
@@ -0,0 +1,135 @@
+package com.weiyin.card_gobook.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Jason Chen on 2017/11/1.
+ */
+
+public class NavigationBarUtils {
+    /**
+     * 是否有虚拟键盘
+     * @param activity
+     * @return
+     */
+    public static boolean hasNavigationBar(Activity activity) {
+        DisplayMetrics dm = new DisplayMetrics();
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        display.getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+        float density = dm.density;
+
+        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealMetrics(realDisplayMetrics);
+        } else {
+            Class c;
+            try {
+                c = Class.forName("android.view.Display");
+                Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+                method.invoke(display, realDisplayMetrics);
+            } catch (Exception e) {
+                realDisplayMetrics.setToDefaults();
+                e.printStackTrace();
+            }
+        }
+
+        int creenRealHeight = realDisplayMetrics.heightPixels;
+        int creenRealWidth = realDisplayMetrics.widthPixels;
+
+        float diagonalPixels = (float) Math.sqrt(Math.pow(screenWidth, 2) + Math.pow(screenHeight, 2));
+        float screenSize = (diagonalPixels / (160f * density)) * 1f;
+
+        Resources rs = activity.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        boolean hasNavBarFun = false;
+        if (id > 0) {
+            hasNavBarFun = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String)m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavBarFun = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavBarFun = true;
+            }
+        } catch (Exception e) {
+            hasNavBarFun = false;
+        }
+        return hasNavBarFun;
+    }
+
+    /**
+     * 是否显示虚拟键盘
+     * @param windowManager
+     * @return
+     */
+    public static boolean checkDeviceHasNavigationBar(WindowManager windowManager) {
+        DisplayMetrics dm = new DisplayMetrics();
+        Display display = windowManager.getDefaultDisplay();
+        display.getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+        return (screenHeight - screenWidth) > 0;//screenRealHeight上面方法中有计算
+    }
+
+    /**
+     * 获取 虚拟按键的高度
+     * @param context
+     * @return
+     */
+    public static  int getBottomStatusHeight(Context context){
+        int totalHeight = getDpi(context);
+
+        int contentHeight = getScreenHeight(context);
+
+        return totalHeight  - contentHeight;
+    }
+
+    //获取屏幕原始尺寸高度，包括虚拟功能键高度
+    public static int getDpi(Context context){
+        int dpi = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        @SuppressWarnings("rawtypes")
+        Class c;
+        try {
+            c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics",DisplayMetrics.class);
+            method.invoke(display, displayMetrics);
+            dpi=displayMetrics.heightPixels;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return dpi;
+    }
+
+    /**
+     * 获得屏幕高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context)
+    {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+}
